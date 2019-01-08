81f1d6b7bfc86df6fe4126af3ee7468e28b3c765
chuang5877 <chuang5877@bths.edu>
2019-01-08 08:29:03 -0500

    selectionSort

--- src/com/company/SelectionSort.java
+++ src/com/company/SelectionSort.java
@@ -1,16 +1,17 @@
 package com.company;
-import com.company.SelectionSort;
+import com.company.SortingUtil;
 
 public class SelectionSort {
     public static int[] selectionSort(int[] arr){
         int m;
         for (int n=0;n<arr.length;n++){
-            m=arr[n];
-            for (int i=1;i<arr.length;i++){
-                if(arr[i]<m){
-                    SortingUtil.swap(arr,n,i);
+            m=n;
+            for (int i=n+1;i<arr.length;i++){
+                if(arr[i]<arr[m]){
+                    m=i;
                 }
             }
+            SortingUtil.swap(arr,m,n);
         }
         return arr;
     }
